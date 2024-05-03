bee3acf309b27c069cc44f951b65c1bb44a7a1e7
pschain <neoguru000@gmail.com>
2024-05-03 06:42:31 +0000

    v0.1.1

--- canister_index.sh
+++ canister_index.sh
@@ -1,1 +1,1 @@
-dfx deploy --playground newPMW_index --argument '(opt variant{Init = record {ledger_id = principal "wfg2r-bqaaa-aaaab-qadfq-cai"}})'
+dfx deploy --playground newPMW_index --argument '(opt variant{Init = record {ledger_id = principal "xblvd-yqaaa-aaaab-qaddq-cai"}})'
---
+++ canister_index_live.sh
@@ -0,0 +1,1 @@
+dfx deploy --network=ic --with-cycles 200000000000 newPMW_index --argument '(opt variant{Init = record {ledger_id = principal "sddoy-iyaaa-aaaam-aclfq-cai"}})'
